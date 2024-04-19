44093699d5cc54b17278cd690b14d1d3724ce12d
DUA-UMER <duam95623@gmail.com>
2024-04-20 01:07:10 +0500

    first commit

---
+++ Q 06/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let whitespaceName = "\n\t Malik Rimsha \t\n";
+console.log(whitespaceName);
+let withoutWhitespaceNmae = whitespaceName.trim();
+console.log(withoutWhitespaceNmae);
---
+++ Q 06/main.ts
@@ -0,0 +1,7 @@
+ let whitespaceName = "\n\t Malik Rimsha \t\n";
+
+  console.log(whitespaceName);
+
+ let withoutWhitespaceNmae = whitespaceName.trim();
+
+ console.log(withoutWhitespaceNmae);
