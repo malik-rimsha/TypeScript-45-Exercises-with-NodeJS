cfa34b3da3009087d1c334f54b464165082d5fc4
DUA-UMER <duam95623@gmail.com>
2024-04-27 20:11:29 +0500

    first commit

---
+++ Q 14/main.js
@@ -0,0 +1,2 @@
+var guestlist = ["Umaima", "Fakhra", "Shazma", "Hadiqa"];
+guestlist.forEach(function (oneGuest) { return console.log("Salam ".concat(oneGuest, ", would you like to dinner with me?")); });
---
+++ Q 14/main.ts
@@ -0,0 +1,3 @@
+let guestlist = ["Umaima", "Fakhra", "Shazma", "Hadiqa"];
+
+guestlist.forEach(oneGuest => console.log(`Salam ${oneGuest}, would you like to dinner with me?`));
