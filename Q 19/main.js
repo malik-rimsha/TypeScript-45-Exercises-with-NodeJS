67a28c08e3a79e35d4593230524ce89a32296921
DUA-UMER <duam95623@gmail.com>
2024-05-18 19:35:29 +0500

    first commit

---
+++ Q 19/main.js
@@ -0,0 +1,4 @@
+var guestlist = ["Umaima", "Fakhra", "Shazma", "Hadiqa", "Sara"];
+// guestlist.forEach(oneGuest => console.log(`Salam ${oneGuest}, would you like to dinner with me?`));
+var lengthGuests = guestlist.length;
+console.log("We are Inviting Total ".concat(lengthGuests, " guests."));
---
+++ Q 19/main.ts
@@ -0,0 +1,7 @@
+let guestlist = ["Umaima", "Fakhra", "Shazma", "Hadiqa","Sara"];
+
+// guestlist.forEach(oneGuest => console.log(`Salam ${oneGuest}, would you like to dinner with me?`));
+
+let lengthGuests : number = guestlist.length;
+
+console.log(`We are Inviting Total ${lengthGuests} guests.`);
