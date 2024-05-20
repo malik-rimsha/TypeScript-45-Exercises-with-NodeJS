cf64458c47bf6a8f29b6813e33ff74a8ec5b53a8
DUA-UMER <duam95623@gmail.com>
2024-05-20 22:00:07 +0500

    first commit

---
+++ Q 20/main.js
@@ -0,0 +1,6 @@
+//making a programming Languages Array 
+var programmingLanguage = ["Typescript", "JavaScript", "python", "PHP"];
+//printing the list of programming Languages
+console.log("List of programming Languages");
+//print the values of array in the form of array
+programmingLanguage.forEach(function (language) { return console.log(language); });
---
+++ Q 20/main.ts
@@ -0,0 +1,8 @@
+//making a programming Languages Array 
+let programmingLanguage : string[] = ["Typescript", "JavaScript", "python", "PHP"];
+
+//printing the list of programming Languages
+console.log("List of programming Languages");
+
+//print the values of array in the form of array
+programmingLanguage.forEach(language => console.log(language));
