f1c7714e2172f3bce6edc76cde4f10fffbcad11f
DUA-UMER <duam95623@gmail.com>
2024-05-22 00:25:48 +0500

    first commit

---
+++ Q 21/main.js
@@ -0,0 +1,6 @@
+var itCourse = {
+    coursName: "TypeScript and JavaScript",
+    location: "Governor House Sindh",
+    onSitestudents: 50000
+};
+console.log(itCourse);
---
+++ Q 21/main.ts
@@ -0,0 +1,13 @@
+interface itCourse {
+    coursName: string; 
+    location: string;
+    onSitestudents: number;
+}
+
+let itCourse = {
+    coursName: "TypeScript and JavaScript",
+    location: "Governor House Sindh",
+    onSitestudents: 50000
+};
+
+console.log(itCourse);
