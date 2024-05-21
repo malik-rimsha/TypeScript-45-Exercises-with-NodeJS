5ee6d0272f988b73e011a1f56134317c34c1369c
DUA-UMER <duam95623@gmail.com>
2024-05-22 01:20:34 +0500

    first commit

---
+++ Q 23/main.js
@@ -0,0 +1,33 @@
+//Making a variables
+var seven = 7;
+var four = 4;
+//Test 1 
+console.log("Test 1: seven is equal to 7?");
+console.log(seven == 7);
+//Test 2
+console.log("\nTest 2: four is equal to 4?");
+console.log(four == 4);
+//Test 3
+console.log("\nTest 3: seven is not equal to 4?");
+console.log(seven != 4);
+//Test 4
+console.log("\nTest 4: seven is greater than 4?");
+console.log(seven > 4);
+//Test 5
+console.log("\nTest 5: four is less than 7?");
+console.log(four < 7);
+//Test 6
+console.log("\nTest 6: seven is less than 4?");
+console.log(seven < 4);
+//Test 7
+console.log("\nTest 7: four is equal to 7?");
+console.log(four == 7);
+//Test 8
+console.log("\nTest 8: seven is not equal to 7?");
+console.log(seven != 7);
+//Test 9
+console.log("\nTest 9: four is greater than 7?");
+console.log(four > 7);
+//Test 10
+console.log("\nTest 10: seven is less than 4?");
+console.log(seven < 4);
