9b28980e15b0e224c0a8855e64323377f34bcd4a
DUA-UMER <duam95623@gmail.com>
2024-05-22 00:46:09 +0500

    first commit

---
+++ Q 22/main.js
@@ -0,0 +1,5 @@
+//creating a Array
+var errorArray = ["E", "F", "G", "H"];
+//Producing Error ! by accessing invalid index of Array
+console.log(errorArray[7]);
+console.log(errorArray[1]);
---
+++ Q 22/main.ts
@@ -0,0 +1,8 @@
+//creating a Array
+let errorArray : string[] = ["E", "F", "G", "H"];
+
+//Producing Error ! by accessing invalid index of Array
+// console.log(errorArray[7]);
+
+//Error removed
+console.log(errorArray[1]);
