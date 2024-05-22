0afbeb483df3ef223ffffc78f51a169cf598375a
DUA-UMER <duam95623@gmail.com>
2024-05-22 23:38:50 +0500

    first commit

---
+++ Q 25/main.js
@@ -0,0 +1,12 @@
+// Define variable
+var alienColor = "Black";
+if (alienColor === "Black") {
+    //code
+    console.log("Alien color is Black player just earned 5 points.");
+}
+;
+if (alienColor === "Gray") {
+    //code
+    console.log("Alien color is Gray.");
+}
+;
