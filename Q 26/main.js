54c4381c538fda719ef58e8bbb3a5c0db305ddce
DUA-UMER <duam95623@gmail.com>
2024-05-22 23:59:00 +0500

    first commit

---
+++ Q 26/main.js
@@ -0,0 +1,18 @@
+// Define variable
+var alienColor = "Green";
+// First version
+if (alienColor === "Green") {
+    console.log("Player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("Player just earned 10 points.");
+}
+;
+// Second version
+if (alienColor === "White") {
+    console.log("I am come from IF Statment.");
+}
+else {
+    console.log("I am come from Else Statment.");
+}
+;
---
+++ Q 26/main.ts
@@ -0,0 +1,20 @@
+// Define variable
+let alienColor = "Green";
+
+// First version
+
+if (alienColor === "Green"){
+    console.log("Player just earned 5 points for shooting the alien.");
+}
+else {
+    console.log("Player just earned 10 points.");
+};
+
+// Second version
+
+if (alienColor === "White"){
+    console.log("I am come from IF Statment.");
+}
+else {
+    console.log("I am come from Else Statment.");
+};
