a120029221e642aa2830ccc43a2132fb2ce90768
DUA-UMER <duam95623@gmail.com>
2024-05-23 22:52:15 +0500

    first commit

---
+++ Q 27/main.ts
@@ -0,0 +1,47 @@
+// Define variables
+let alienColor = "Green";
+
+// // version 1
+
+// // Using If and else_IF statement 
+if (alienColor === "Green") {
+    console.log("(version 1 ) you shot down Green Alien  you have earnd 5 points.");
+}
+else if (alienColor === "Yellow") {
+    console.log("you shot down yellow Alien  you have earnd 10 points.");
+}
+else if (alienColor === "Red"){
+    console.log("you shot down Red Alien  you have earnd 15 points.");
+};
+
+// Version 2
+ 
+let alienColor2 = "Yellow";
+
+if (alienColor2 === "Green") {
+    console.log("you shot down Green Alien  you have earnd 5 points.");
+}
+else if (alienColor2 === "Yellow"){
+    console.log("(version 2 ) you shot down yellow Alien  you have earnd 10 points.");
+}
+else if (alienColor2 === "Red"){
+    console.log("you shot down Red Alien  you have earnd 15 points.");
+};
+
+// Version 3
+let alienColor3 = "Red";
+
+ if (alienColor3 === "Green"){
+    console.log("you shot down Green Alin you have earned 5 points.");
+}
+else if (alienColor3 === "Yellow"){
+    console.log("you shot down Yellow Alien you have earend 10 points.");
+}
+else if (alienColor3 === "Red"){
+    console.log("(version 3 ) you shot down Red Alien you have earned 15 points.");
+}
+
+
+
+
+
