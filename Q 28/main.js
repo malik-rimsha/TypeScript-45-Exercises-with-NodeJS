4a24e5045f5b42b8392945f7b3936a9f6529e509
DUA-UMER <duam95623@gmail.com>
2024-05-23 23:24:54 +0500

    first commit

---
+++ Q 28/main.js
@@ -0,0 +1,20 @@
+// Creat a variable
+var age = 66;
+if (age < 2) {
+    console.log("you are a Baby.");
+}
+else if (age >= 2 && age < 4) {
+    console.log("you are a Toddler.");
+}
+else if (age >= 4 && age < 13) {
+    console.log("you are a Kid.");
+}
+else if (age >= 13 && age < 20) {
+    console.log("you are a Teenager.");
+}
+else if (age >= 20 && age < 65) {
+    console.log("you are an Adult.");
+}
+else if (age >= 65) {
+    console.log("you are an Elder.");
+}
