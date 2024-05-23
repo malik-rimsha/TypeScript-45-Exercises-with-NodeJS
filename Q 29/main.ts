22fa7bcecb041034bedad33fd272e6355bde6573
DUA-UMER <duam95623@gmail.com>
2024-05-24 00:14:44 +0500

    first commit

---
+++ Q 29/main.ts
@@ -0,0 +1,20 @@
+// Creat a Array
+let favorite_fruit = ["Mango", "Cherry", "Orange"]
+
+// Using 5 Independent If Statements
+
+if (favorite_fruit.includes("Mango")){
+    console.log("I really like Mangos");
+}
+if (favorite_fruit.includes("Cherry")){
+    console.log("I really like Cherrys");
+}
+if (favorite_fruit.includes("Grapes")){
+    console.log("I really like Grapes");
+}
+if (favorite_fruit.includes("Orange")){
+    console.log("I really like Oranges");
+}
+if (favorite_fruit.includes("Guava")){
+    console.log("I really like Guavas");
+}
