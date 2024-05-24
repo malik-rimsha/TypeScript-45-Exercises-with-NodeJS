4fcff447c39a6b3c714cd36780ff37d3ae18a182
DUA-UMER <duam95623@gmail.com>
2024-05-25 00:53:18 +0500

    first commit

---
+++ Q 35/main.js
@@ -0,0 +1,10 @@
+// Create a Array
+var petAnimals = ["Cat", "Dog", "Rabbit"];
+// Using For_Loop
+for (var _i = 0, petAnimals_1 = petAnimals; _i < petAnimals_1.length; _i++) {
+    var onePet = petAnimals_1[_i];
+    console.log("A ".concat(onePet, " would make a great pet."));
+}
+;
+// Print  a message outside of For_Loop
+console.log("Any of these animals would make a great pet.");
