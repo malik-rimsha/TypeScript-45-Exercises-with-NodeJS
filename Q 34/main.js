c08bfd1c5ecbabbc3fd73cfdd66c6158de8b824d
DUA-UMER <duam95623@gmail.com>
2024-05-25 00:37:29 +0500

    first commit

---
+++ Q 34/main.js
@@ -0,0 +1,10 @@
+// Creating a Array
+var pizza = ["Chicken Tika", "Malai Cheez", "Fajita"];
+// Using For_Loop
+for (var _i = 0, pizza_1 = pizza; _i < pizza_1.length; _i++) {
+    var onePizza = pizza_1[_i];
+    console.log("I Like ".concat(onePizza, " pizza"));
+}
+// Print a message outside of the For_Loop
+console.log("Pizza is love.");
+console.log("I eat Pizza.");
---
+++ Q 34/main.ts
@@ -0,0 +1,11 @@
+// Creating a Array
+let pizza = ["Chicken Tika", "Malai Cheez", "Fajita"];
+
+// Using For_Loop
+for(let onePizza of pizza){
+    console.log(`I Like ${onePizza} pizza`);
+}
+
+// Print a message outside of the For_Loop
+console.log("Pizza is love.");
+console.log("I eat Pizza.")
