fa460b35c6a93fb1c0ba233fbe2972e45249edfc
DUA-UMER <duam95623@gmail.com>
2024-05-25 03:10:53 +0500

    first commit

---
+++ Q 40/main.ts
@@ -0,0 +1,28 @@
+// Define the Make_album Function
+function make_album(artist_name: string, album_title: string,tracks?: number){
+    let album:{artist: string, title: string, tracks?: number} ={
+        artist: artist_name,
+        title: album_title,
+    };
+
+    if(tracks !== undefined){
+        album.tracks = tracks;
+    }
+
+    return album;
+};
+
+// Calling three functions and creating three variables with different values
+let album1 = make_album("Malik", "Album title 1");
+
+let album2 = make_album("Rimsha", "Album title 2");
+
+// Calling a make_album function with third parameter
+let album3 = make_album("Miral", "Album title 3", 7);
+
+// Printing values of our object created my function
+console.log(album1);
+
+console.log(album2);
+
+console.log(album3);
